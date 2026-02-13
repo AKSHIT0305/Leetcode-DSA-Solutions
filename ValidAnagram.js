f85229c3c533e1a03fbca1c908b44827f78e15a4
Akshit Goel <akshit.goel.03@gmail.com>
2026-02-13 22:22:28 +0530

    valid anagram

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -20,3 +20,25 @@
 
 // 1 <= s.length, t.length <= 5 * 104
 // s and t consist of lowercase English letters.
+
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const counter = new Map();
+
+    for (let char of s) {
+        counter.set(char, (counter.get(char) || 0) + 1);
+    }
+
+    for (let char of t) {
+        if (!counter.has(char) || counter.get(char) === 0) {
+            return false;
+        }
+        counter.set(char, counter.get(char) - 1);
+    }
+
+    return true;    
+};
