6f1f570f3b3e660d1cb75b9a97830257a8e8a73c
Akshit Goel <akshit.goel.03@gmail.com>
2026-02-15 22:28:53 +0530

    Remove element

--- RemoveElement.js
+++ RemoveElement.js
@@ -46,3 +46,15 @@
 // 0 <= val <= 100
 
 
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;    
+};
