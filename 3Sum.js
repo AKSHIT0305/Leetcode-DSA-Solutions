40ab77f42b3ccab9b7769b39c3ab858da2076fa2
Akshit Goel <akshit.goel.03@gmail.com>
2026-02-20 23:24:34 +0530

    3 sum

--- 3Sum.js
+++ 3Sum.js
@@ -26,3 +26,25 @@
 // -105 <= nums[i] <= 105
 
 
+var threeSum = function(nums) {
+    const res = [];
+    nums.sort((a,b) => a-b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i && nums[i] === nums[i - 1]) continue;
+
+        let l = i + 1, r = nums.length - 1;
+
+        while (l < r) {
+            const sum = nums[i] + nums[l] + nums[r];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[l++], nums[r--]]);
+                while (l < r && nums[l] === nums[l - 1]) l++;
+                while (l < r && nums[r] === nums[r + 1]) r--;
+            } 
+            else sum < 0 ? l++ : r--;
+        }
+    }
+    return res;
+};
